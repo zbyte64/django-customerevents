beafbfa5900d1b13bcab031b8bf92ed5968b9e10
Jason Kraus <zbyte64@gmail.com>
2014-01-24 12:17:12 -0800

    improved kiss metrics integration, version bump

--- customerevents/backends/kissmetrics.py
+++ customerevents/backends/kissmetrics.py
@@ -5,6 +5,25 @@ from KISSmetrics import KM
 from .base import BaseBackend
 
 
+def normalize_name(name):
+    '''
+    KissMetrics likes human readable names, not keywords.
+    '''
+    return name.replace('.', ' ')
+
+
+def normalize_identity(identity, aliases, properties):
+    '''
+    If the customer has an email we use that as their identity.
+    This may obviously change from one use case to another so this should be
+    configurable.
+    '''
+    if properties.get('email'):
+        aliases.add(identity)
+        identity = properties['email']
+    return identity, aliases, properties
+
+
 class KissMetricsBackend(BaseBackend):
     template_name = 'customerevents/kissmetrics.html'
 
@@ -13,23 +32,28 @@ class KissMetricsBackend(BaseBackend):
         self.api_key = API_KEY
         super(KissMetricsBackend, self).__init__(**kwargs)
 
-    def get_context(self, identity, properties, events, **kwargs):
+    def get_context(self, identity, aliases, properties, events, **kwargs):
+        identity, aliases, properties = normalize_identity(identity, aliases, properties)
         context = {
             'identify': identity,
             'properties': json.dumps(properties),
             'record': list(),
             'api_key': self.api_key,
+            'aliases': aliases,
         }
         context.update(kwargs)
         for event_name, event_properties in events:
-            context['record'].append((event_name, json.dumps(event_properties)))
+            context['record'].append((normalize_name(event_name),
+                                      json.dumps(event_properties)))
         return context
 
     def send(self, identity, properties, aliases, events, request_meta):
+        identity, aliases, properties = normalize_identity(identity, aliases, properties)
         self.connection.identify(identity)
         for alias in aliases:
             self.connection.alias(identity, alias)
         if properties:
             self.connection.set(properties)
         for event_name, event_properties in events:
-            self.connection.record(event_name, event_properties)
+            self.connection.record(normalize_name(event_name),
+                                   event_properties)
--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except:
 
 setup(
     name='django-customerevents',
-    version='0.0.5',
+    version='0.0.6',
     author='Jason Kraus',
     author_email='zbyte64@gmail.com',
     packages=find_packages(exclude=['tests']),
